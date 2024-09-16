6a9a94f3f7c7f387968d9c2e8e60d00246560503
Krzysztof Siwek <krzysztof.siwek@programisci.eu>
2024-09-16 07:28:04 +0200

    Init project with connected database in MySQL

---
+++ src/main/java/com/siwyus/qrcontainer/QRContainerApplication.java
@@ -0,0 +1,13 @@
+package com.siwyus.qrcontainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QRContainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QRContainerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/siwyus/qrcontainer/QRContainerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.siwyus.qrcontainer;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QRContainerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
